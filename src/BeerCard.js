d092473889404712c93653a1a73307c168f87834
Victor Lee <myleevictor@gmail.com>
2018-03-27 15:46:13 +0800

    added Bulma stylesheet

--- src/BeerCard.js
+++ src/BeerCard.js
@@ -4,7 +4,7 @@ const BeerCard = props => {
   return (
     <div className="beer-Card">
       <img id="beerCardGraphic" src={props.imageUrl} alt="An awesome beer" />
-      <h1>{props.name}</h1>
+      <h1 className="title">{props.name}</h1>
       <h2>{props.description}</h2>
       <h3>{props.food_pairing}</h3>
     </div>
